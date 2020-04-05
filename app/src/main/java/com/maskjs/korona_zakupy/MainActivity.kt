09006312f964b4f57e60e5bfb23a7fa7e561fc28
Vekon <s.kaczmarek084@gmail.com>
2020-04-05 21:06:20 +0200

    first commit

---
+++ app/src/main/java/com/maskjs/korona_zakupy/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.maskjs.korona_zakupy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
