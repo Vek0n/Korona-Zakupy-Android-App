1f1416160fe0d1b21ec7c543f8ad0ddf307b4250
Vek0n <s.kaczmarek084@gmail.com>
2020-05-12 13:53:31 +0200

    fixed active orders fragment dialog

---
+++ app/src/main/java/com/maskjs/korona_zakupy/volunteer_ui/OrderDetailsDialog.kt
@@ -0,0 +1,16 @@
+package com.maskjs.korona_zakupy.volunteer_ui
+
+import android.app.Activity
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.view.Window
+import com.maskjs.korona_zakupy.R
+
+class OrderDetailsDialog(var activity: Activity,
+                         internal var adapter: OrdersListAdapter
+): Dialog(activity) {
+
+
+
+}
