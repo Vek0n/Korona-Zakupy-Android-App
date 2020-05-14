8d90f1ad93ffe9b0b43392fae885e46d5fcf28d0
kivi221 <kivi9831@gmail.com>
2020-05-14 18:35:30 +0200

    Finished working on login activity (it works)

--- app/src/main/java/com/maskjs/korona_zakupy/LoginActivity.kt
+++ app/src/main/java/com/maskjs/korona_zakupy/LoginActivity.kt
@@ -5,11 +5,8 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.preference.PreferenceManager
 import androidx.activity.viewModels
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.edit
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
 import com.maskjs.korona_zakupy.databinding.ActivityLoginBinding
 import com.maskjs.korona_zakupy.viewmodels.login.LoginViewModel
@@ -19,64 +16,27 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import org.jetbrains.anko.share
-import kotlin.math.log
+import kotlin.Exception
 
 class LoginActivity : AppCompatActivity() {
     private val loginViewModel : LoginViewModel by viewModels()
     private lateinit var uiDataBinding: ActivityLoginBinding
     private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-        setSupportActionBar(toolbar)
 
-        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        initialize()
 
-        initializeUiDataBinding()
         observeUiElements()
 
-        uiDataBinding.textViewToRegister.setOnClickListener{
-          goToRegisterActivity()
-        }
-
-        uiDataBinding.fab.setOnClickListener {
-            if(validateUi()){
-                CoroutineScope(Dispatchers.IO).launch {
-                    login()
-                }
-                handleRegisterResponse()
-            }
-        }
+        setUiListener()
     }
 
-    private suspend fun login(){
-        loginViewModel.login()
-    }
-
-    private fun handleRegisterResponse(){
-        saveResponse()
-        goToUserActivity()
-    }
-
-    private fun saveResponse(){
-        val editor= sharedPreferences.edit()
-            editor.putString(R.string.user_id_key.toString(), loginViewModel.loginResponseDto.userId)
-            editor.putString(getString(R.string.user_token_key),loginViewModel.loginResponseDto.token)
-            editor.commit()
-    }
-
-    private fun goToUserActivity(){
-        val intent = Intent(this, VolunteerActivity::class.java)
-        this?.startActivity(intent)
-    }
-
-    private fun goToRegisterActivity(){
-        val intent = Intent(this, RegisterActivity::class.java)
-        startActivity(intent)
-    }
-
-    private fun initializeUiDataBinding(){
+    private fun initialize(){
+        setContentView(R.layout.activity_login)
+        setSupportActionBar(toolbar)
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
         uiDataBinding = DataBindingUtil.setContentView(this,R.layout.activity_login)
         uiDataBinding.loginViewModel = loginViewModel
         uiDataBinding.lifecycleOwner = this
@@ -89,6 +49,8 @@ class LoginActivity : AppCompatActivity() {
 
     private fun observeEmail(){
         loginViewModel.emailEditTextContent.observe(this, Observer {
+            uiDataBinding.emailTextEditText.error = null
+
             if(!loginViewModel.isNotEmpty(it))  uiDataBinding.emailTextInputLayout.error = getString(
                 R.string.global_empty_field_error) else {
                 uiDataBinding.emailTextInputLayout.error = null
@@ -98,11 +60,39 @@ class LoginActivity : AppCompatActivity() {
 
     private fun observePassword(){
         loginViewModel.passwordEditTextContent.observe(this, Observer {
+            uiDataBinding.passwordEditText.error = null
+
             if(!loginViewModel.isNotEmpty(it))  uiDataBinding.passwordTextInputLayout.error = getString(
                 R.string.global_empty_field_error) else {
                 uiDataBinding.passwordTextInputLayout.error = null
             }
         })
+    }
+
+    private fun setUiListener(){
+        setTextViewToRegisterListener()
+        setFabListener()
+    }
+
+    private fun setTextViewToRegisterListener(){
+        uiDataBinding.textViewToRegister.setOnClickListener{
+            goToRegisterActivity()
+        }
+    }
+
+    private  fun setFabListener(){
+        uiDataBinding.fab.setOnClickListener {
+            if(validateUi()){
+                CoroutineScope(Dispatchers.IO).launch {
+                    login()
+                }
+            }
+        }
+    }
+
+    private fun goToRegisterActivity(){
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
     }
 
     private fun validateUi() : Boolean {
@@ -121,4 +111,38 @@ class LoginActivity : AppCompatActivity() {
         return check
     }
 
+    private suspend fun login(){
+        try {
+            loginViewModel.login()
+            withContext(Dispatchers.Main){
+                handleLoginResponse()
+            }
+        }catch (ex : Exception){
+            withContext(Dispatchers.Main) {
+                handleException()
+            }
+        }
+    }
+
+    private fun handleLoginResponse(){
+        saveResponse()
+        goToUserActivity()
+    }
+
+    private fun saveResponse(){
+        val editor= sharedPreferences.edit()
+        editor.putString(R.string.user_id_key.toString(), loginViewModel.loginResponseDto.userId)
+        editor.putString(getString(R.string.user_token_key),loginViewModel.loginResponseDto.token)
+        editor.commit()
+    }
+
+    private fun goToUserActivity(){
+        val intent = Intent(this, VolunteerActivity::class.java)
+        this?.startActivity(intent)
+    }
+
+    private fun handleException(){
+        uiDataBinding.emailTextEditText.error = "Email or password invalid"
+        uiDataBinding.passwordEditText.error = "Email or password invalid"
+    }
 }
