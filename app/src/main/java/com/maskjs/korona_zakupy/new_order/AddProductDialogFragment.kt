f46390150bf0178dea03b8c2ad335b533ce60229
kivi221 <kivi9831@gmail.com>
2020-05-25 13:22:14 +0200

    Added correctly dialog (needs only a little refactoring)

--- app/src/main/java/com/maskjs/korona_zakupy/new_order/AddProductDialogFragment.kt
+++ app/src/main/java/com/maskjs/korona_zakupy/new_order/AddProductDialogFragment.kt
@@ -21,42 +21,72 @@ class AddProductDialogFragment : DialogFragment() {
 
     private  var onAddProductClickListener : OnAddProductClickListener? = null
     private lateinit var dialogBinding: FragmentDialogAddProductBinding
-    private lateinit var addProductDialogBuilder : AlertDialog.Builder
     private val addProductDialogViewModel: AddProductDialogViewModel by viewModels{
         AddProductDialogViewModelFactory(resources.getStringArray(R.array.dialog_number_picker_values),
             resources.getStringArray(R.array.dialog_number_picker_unit)) }
-   // private lateinit var addProductDialogViewModel: AddProductDialogViewModel
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
 
         onAddProductClickListener = (context as? OnAddProductClickListener)
-       // addProductDialogViewModel = AddProductDialogViewModel(resources.getStringArray(R.array.dialog_number_picker_values),
-        //    resources.getStringArray(R.array.dialog_number_picker_unit))
 
         if(onAddProductClickListener == null)
             throw ClassCastException("Error!")
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         activity?.layoutInflater?.let { setLayoutBinding(it,  null)}
         setProductObserver()
 
-        addProductDialogBuilder = activity?.let { AlertDialog.Builder(it) }!! // could be Error ?
+        val addProductDialog = buildDialog()
+
+        setDialogClickListeners(addProductDialog)
+
+        return addProductDialog
+    }
+
+    private fun setLayoutBinding(inflater: LayoutInflater,container: ViewGroup?){
+        dialogBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_dialog_add_product,container,false)
+        dialogBinding.addProductDialogViewModel = addProductDialogViewModel
+    }
+
+    private fun setProductObserver(){
+        addProductDialogViewModel.productTextInputLayout.textContent.observe(this, Observer {
+            if( !addProductDialogViewModel.checkValidation(mapOf(Pair("emptyError",getString(R.string.global_empty_field_error))))){
+                dialogBinding.dialogLayoutTextInputProductName.error = getString(R.string.global_empty_field_error)
+            }
+            else{
+                dialogBinding.dialogLayoutTextInputProductName.error = null
+            }
+        })
+    }
+
+    private fun setDialogClickListeners(addProductDialog: AlertDialog){
+        dialogBinding.dialogAcceptAddProduct.setOnClickListener {
+            onPositiveDialogButtonClickedListener(addProductDialog)
+        }
+
+        dialogBinding.dialogCancelAddProduct.setOnClickListener {
+            dismiss()
+        }
+    }
+
+    private fun buildDialog(): AlertDialog{
+        val addProductDialogBuilder = activity?.let { AlertDialog.Builder(it) }!!
         addProductDialogBuilder.setView(dialogBinding.root)
-        addProductDialogBuilder.setTitle(getString(R.string.text_view_add_new_product)) //getString(R.string.text_view_add_new_product)
-        addProductDialogBuilder.setPositiveButton(getString(R.string.dialog_positive_button_text)) { dialog, xd ->
-            onPositiveDialogButtonClickedListener() }
-        addProductDialogBuilder.setNegativeButton(getString(R.string.dialog_negative_button_text)){_,_-> }
+        addProductDialogBuilder.setTitle(getString(R.string.text_view_add_new_product))
+
         return addProductDialogBuilder.create()
     }
 
-    private fun onPositiveDialogButtonClickedListener(){
+    private fun onPositiveDialogButtonClickedListener(addProductDialog: AlertDialog){
+        if(!addProductDialogViewModel.checkValidation(mapOf(Pair("emptyError",getString(R.string.global_empty_field_error))))) {
+            dialogBinding.dialogLayoutTextInputProductName.error = getString(R.string.global_empty_field_error)
+        }
+        else{
             onAddProductClickListener?.addProduct(addProductDialogViewModel)
+            addProductDialog.dismiss()
+        }
     }
 
 
@@ -68,27 +98,7 @@ class AddProductDialogFragment : DialogFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-          //  setLayoutBinding(inflater, container)
-           // setProductObserver()
-
             return dialogBinding.root
-        }
-
-    private fun setLayoutBinding(inflater: LayoutInflater,container: ViewGroup?){
-        dialogBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_dialog_add_product,container,false)
-        dialogBinding.addProductDialogViewModel = addProductDialogViewModel
     }
-
-    private fun setProductObserver(){
-        addProductDialogViewModel.productTextInputLayout.textContent.observe(this, Observer {
-           if( addProductDialogViewModel.checkValidation(mapOf(Pair("emptyError",getString(R.string.global_empty_field_error))))){
-               dialogBinding.dialogLayoutTextInputProductName.error = getString(R.string.global_empty_field_error)
-           }
-            else{
-               dialogBinding.dialogLayoutTextInputProductName.error = null
-           }
-        })
-    }
-
-    }
+}
 
